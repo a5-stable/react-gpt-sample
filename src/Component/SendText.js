74d473498585da6fc8fa642ecbf994c24a749942
mizuno <satihase1123@gmail.com>
2023-03-18 20:44:17 +0900

    implement push ctrl + Enter then send

--- src/Component/SendText.js
+++ src/Component/SendText.js
@@ -1,33 +1,53 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 
 const SendText = (props) => {
-  const [textareaHeight, setTextareaHeight] = useState("30px");
+  // textareaの高さの初期値
+  const textareaHeight = "44px";
+
+  const textAreaRef = useRef(null);
 
   // テキストエリア変化
   const handleTextareaChange = (event) => {
     props.setInputValue(event.target.value);
     // textarea の行数を計算しサイズを自動調整
-    event.target.style.height = "30px";
-    setTextareaHeight(`${event.target.scrollHeight}px`);
+    event.target.style.height = "auto";
+    event.target.style.height = `${event.target.scrollHeight}px`;
   };
 
-  // ボタンクリック時テキストエリアのサイズ初期化
+  // ボタンクリック時
   const handleButtonClick = () => {
-    setTextareaHeight("30px");
+    var textarea = document.getElementById("send-textarea");
+    textarea.style.height = "44px"; // テキストエリアの高さを50pxに変更
+    textAreaRef.current.focus(); // テキストエリアにフォーカスを当てる
+  };
+
+  // Ctrl+EnterまたはCommand+Enterでメソッド呼び出し
+  const handleKeyDown = (event) => {
+    if ((event.ctrlKey || event.metaKey) && event.keyCode === 13) {
+      // Ctrl+EnterまたはCommand+Enterで送信ボタンと同様の動作
+      props.onClickSend();
+      handleButtonClick();
+    }
   };
 
   return (
     <div className="SendText">
       <div className="msgText">※会話の履歴は10往復まで保存されます。</div>
       <textarea
+        id="send-textarea"
+        ref={textAreaRef}
         type="text"
         value={props.inputValue}
         onChange={(e) => {
           handleTextareaChange(e);
         }}
-        style={{ height: textareaHeight }}
+        style={{
+          height: textareaHeight,
+          backgroundColor: props.isCommunicating ? "gray" : "white",
+        }}
         className="txtarea-send"
-        disabled={props.isCommunicating}
+        readOnly={props.isCommunicating}
+        onKeyDown={handleKeyDown}
       ></textarea>
       <div>
         <button
